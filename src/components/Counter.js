b66e148e192835068fa46af7d0c08aff6af3bb12
boramyy <boramyyo@gmail.com>
2020-02-19 14:55:19 +0900

    Add project

---
+++ src/components/Counter.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  render() {
+    return (
+      <div>
+        <h1>counter!</h1>
+      </div>
+    );
+  }
+}
+
+export default Counter;
